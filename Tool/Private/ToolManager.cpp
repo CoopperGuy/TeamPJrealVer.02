a0762c965548000c119866eccf9297ccfaec894b
Seoseongyeon <87797199+Seoseongyeon@users.noreply.github.com>
2022-03-22 21:32:58 +0900

    0932

--- Tool/Private/ToolManager.cpp
+++ Tool/Private/ToolManager.cpp
@@ -23,7 +23,7 @@ USING(Tool)
 
 extern HINSTANCE hInst;
 
-static string strScene = "../../Assets/Scenes/Boss_Ursa.yaml";
+static string strScene = "../../Assets/Scenes/CityMap.yaml";
 
 
 CToolManager::CToolManager()
