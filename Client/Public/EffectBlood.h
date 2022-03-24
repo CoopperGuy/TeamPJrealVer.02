e2c1bce70349243cd4734407bc7c7f5e79b41557
Seoseongyeon <87797199+Seoseongyeon@users.noreply.github.com>
2022-03-24 22:16:23 +0900

    blood

--- Client/Public/EffectBlood.h
+++ Client/Public/EffectBlood.h
@@ -12,14 +12,21 @@ private:
 	explicit CEffectBlood();
 	virtual ~CEffectBlood() = default;
 public:
-	virtual HRESULT Initialize(void* pArg);
+	virtual HRESULT Initialize(void* pArg, _vector pos);
 	virtual void Update(_double deltaTime) ;
 	virtual void LateUpdate(_double deltaTime) ;
 	virtual void Render();
 
 public:
-	static CEffectBlood* Create(void*	pTarget);
+	static CEffectBlood* Create(void*	pTarget, _vector pos);
 	virtual void Free() override;
+
+private:
+	_double m_dDeadTime = 0;
+
+	_float3 Startscail = {};
+	_float ScailX = 0.f;
+	_float ScailY = 0.f;
 };
 
 END
