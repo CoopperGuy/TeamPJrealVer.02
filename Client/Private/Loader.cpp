a0bed71d1fed365adcddc08318de5f891aca9aac
Seoseongyeon <crisente235@gmail.com>
2022-03-29 12:09:53 +0900

    asdasd
    asdasdas

--- Client/Private/Loader.cpp
+++ Client/Private/Loader.cpp
@@ -227,8 +227,13 @@ HRESULT CLoader::GameSceneStage02()
 	CEmptyGameObject* pPlayer = static_cast<CEmptyGameObject*>(CEngine::GetInstance()->FindGameObjectWithName(SCENE_STATIC, "Player"));
 	static_cast<CCollider*>(pPlayer->GetComponent("Com_Collider"))->SetPosition(_float3(0.f, 0.5f, -22.f));
 
-	m_ThreadLoader->EnqueueJob(ThreadTest, this, "../../Assets/Scenes/Flogas_Dungeon.yaml", SCENE_STAGE2, 0);
-	m_ThreadLoader->EnqueueJob(ThreadTest, this, "../../Assets/Scenes/Flogas.yaml", SCENE_STAGE2, 1);
+	CEngine::GetInstance()->DeserializeScene("../../Assets/Scenes/Flogas_Dungeon.yaml", SCENE_STAGE2);
+	m_pLoadingGauge->SetPercentage(50.f);
+	CEngine::GetInstance()->DeserializeScene("../../Assets/Scenes/Flogas.yaml", SCENE_STAGE2);
+	m_pLoadingGauge->SetPercentage(60.f);
+
+	//m_ThreadLoader->EnqueueJob(ThreadTest, this, "../../Assets/Scenes/Flogas_Dungeon.yaml", SCENE_STAGE2, 0);
+	//m_ThreadLoader->EnqueueJob(ThreadTest, this, "../../Assets/Scenes/Flogas.yaml", SCENE_STAGE2, 1);
 
 
 	if (FAILED(GameFlogasLoader()))
