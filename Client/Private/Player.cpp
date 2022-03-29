5ed2c758fd586b69be39fbf30dd11592fb42152d
Seoseongyeon <87797199+Seoseongyeon@users.noreply.github.com>
2022-03-29 16:06:42 +0900

    피주석

--- Client/Private/Player.cpp
+++ Client/Private/Player.cpp
@@ -241,7 +241,8 @@ void CPlayer::Update(_double dDeltaTime)
 	Collsion();
 	Transform_ToWorldSpace();
 	SearchMonster();
-	CreateBlood();
+	//if (m_pStatus->GetStatInfo().hp >= 0)
+	//	CreateBlood();
 	SlowMotion(dDeltaTime);
 	SlowAttack(dDeltaTime);
 }
