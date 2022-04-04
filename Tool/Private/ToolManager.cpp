3904cb07cb6b0abb9494062157cbd206c43d1678
Seoseongyeon <87797199+Seoseongyeon@users.noreply.github.com>
2022-04-04 21:22:59 +0900

    444

--- Tool/Private/ToolManager.cpp
+++ Tool/Private/ToolManager.cpp
@@ -23,7 +23,7 @@ USING(Tool)
 
 extern HINSTANCE hInst;
 
-static string strScene = "../../Assets/Scenes/DKNightDungeon.yaml";
+static string strScene = "../../Assets/Scenes/DarkKnightDungeon.yaml";
 
 CToolManager::CToolManager()
 	: m_pEngine(CEngine::GetInstance())
